4c280db0d9443065ffe833ae9d2e74ea7d7f5f4b
sokmean-mptc <ngonsokmean@mptc.gov.kh>
2021-07-04 08:50:32 +0700

    #

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,7 +8,7 @@
         <form action="/" method="GET">
           <div class="input-group mb-0">
             <input name="s" type="search" class="form-control" placeholder="{{ __( 'Key Word', 'sage' ) }}" aria-label="{{ __( 'SEARCH', 'sage' ) }}" aria-describedby="button-search">
-            <button class="btn btn-outline-secondary bg-gradient color-white" type="submit" id="button-search">{{ __( 'SEARCH', 'sage' ) }}</button>
+            <button class="btn btn-outline bg-gradient color-white" type="submit" id="button-search">{{ __( 'SEARCH', 'sage' ) }}</button>
           </div>
         </form>
       </div>
